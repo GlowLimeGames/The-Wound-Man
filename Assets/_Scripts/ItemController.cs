342277616a520caa5a8f84a5ca801038fa343d19
hollowaytape <hollowaytape@retro-type.com>
2018-02-14 02:21:55 -0500

    Can use an item on a target, destroying both

--- Assets/_Scripts/ItemController.cs
+++ Assets/_Scripts/ItemController.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour {
 
-	public Transform tooltip;
+    private GameController _gm;
+    public Transform tooltip;
 	public Canvas canv;
 	private Transform activeTooltip;
+    private bool _onMouse;
+    private Camera _cam;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        _cam = Camera.main;
+        _onMouse = false;
+        _gm = GameObject.Find("GameController").GetComponent<GameController>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (_onMouse)
+        {
+            // Item moves with the cursor
+            Vector3 mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0.0f;                // Otherwise it gets set to -10 for some reason, and becomes invisible
+            mousePos.y = mousePos.y + 1.0f;   // Item moves slightly above the cursor
+            transform.position = mousePos;
+        }
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        print("Colliding with something");
+        if (coll.gameObject.tag == "SwordTarget")
+        {
+            print("Collided with target");
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (!_onMouse)
+        {
+            _onMouse = true;
+            _gm.itemOnMouse = this;
+
+        } else
+        {
+            _onMouse = false;
+            _gm.itemOnMouse = null;
+        }
+    }
 
 	void OnMouseOver() {
 
@@ -36,6 +71,13 @@ public class ItemController : MonoBehaviour {
 	}
 
 	void OnMouseExit() {
-		//Destroy (activeTooltip.gameObject);
-	}
+        if (!_onMouse)
+        {
+            Destroy(activeTooltip.gameObject);
+        }
+    }
 }
+
+// When I click on the target, check if the right object is "on the mouse"
+// (But currently if I click on anything, it clicks on the on-mouse object...)
+// For now I'll just make the item track above the mouse
---
+++ Assets/_Scripts/TargetController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+    private GameController _gm;
+
+	// Use this for initialization
+	void Start () {
+        _gm = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        // TODO Don't hard-code this
+        if (_gm.itemOnMouse.CompareTag("Sword"))
+        {
+            // TODO: Do things with animus
+            Destroy(_gm.itemOnMouse.gameObject);
+            _gm.itemOnMouse = null;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Player items should have a boxcollider2D and a rigidbody2D set to d ynamic, with a gravity scale 0.
+        // That's one way to get the triggers to happen
+        print(other);
+    }
+}
