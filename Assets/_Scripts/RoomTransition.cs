7fc2e24fa4b94ab63f3fe22866be2e74307907ab
Max Silbiger <hollowaytape@retro-type.com>
2018-02-20 22:00:27 -0500

    Move camera between rooms in the same scene

---
+++ Assets/_Scripts/RoomTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour {
+
+	public Room destinationRoom;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseUp() {
+		Map.Instance.ChangeRoom (destinationRoom);
+	}
+}
